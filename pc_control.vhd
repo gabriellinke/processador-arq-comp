e5ddc69e7e640a7b6cc28f310b93c6e2d82358de
Gabriel Henrique Linke <gh.linke@hotmail.com>
2022-05-05 00:24:12 -0300

    Atualiza pc_control

--- pc_control.vhd
+++ pc_control.vhd
@@ -11,26 +11,28 @@ entity pc_control is
     );    
 end entity pc_control;
 
+
 architecture a_pc_control of pc_control is
     component reg16bits
-        port(
-            clk : in std_logic;
+    port(
+        clk : in std_logic;
             reset : in std_logic;
             wr_en : in std_logic;
             data_in : in signed(15 downto 0);
             data_out : out signed(15 downto 0)
-        );
+            );
     end component;
+    signal resultado_incremento: signed(15 downto 0) := data_in;
 begin
     PC: reg16bits port map(  
         clk => clk,
         reset => '0', -- acho que não precisa ter reset no pc
         wr_en => wr_en,
-        data_in => data_in,
+        data_in => resultado_incremento,
         data_out => data_out
     );
 
-    data_out <=   data_in + 1 when wr_en = '1' else
-                   data_in;
+    resultado_incremento <=   resultado_incremento + 1 when wr_en = '1' and rising_edge(clk) else
+                   resultado_incremento;
 
 end architecture a_pc_control;
