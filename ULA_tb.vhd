a2e6d3b9c0d5525dcfd8e572a1b4e97dfa8df871
Gabriel Henrique Linke <gh.linke@hotmail.com>
2022-05-15 23:10:20 -0300

    Melhora testbench da ULA

--- ULA_tb.vhd
+++ ULA_tb.vhd
@@ -15,9 +15,9 @@ architecture a_ULA_tb of ULA_tb is
             );
     end component;
     
-    signal input1, input2, output1 : signed(15 downto 0);
-    signal opselect : unsigned(1 downto 0);
-    signal output_greater_equal_or_signal, output_zero : std_logic;
+    signal input1, input2, output1 : signed(15 downto 0) := "0000000000000000";
+    signal opselect : unsigned(1 downto 0) := "00";
+    signal output_greater_equal_or_signal, output_zero : std_logic := '0';
     ----------------------
     ------ opselect ------
     -- 00 sum
@@ -26,20 +26,86 @@ architecture a_ULA_tb of ULA_tb is
     -- 11 and
     ----------------------
 begin
-    uut: ULA port map(  input1 => input1,
-                        input2 => input2,
-                        opselect => opselect,
-                        output1 => output1,
-                        output_greater_equal_or_signal => output_greater_equal_or_signal,
-                        output_zero => output_zero
-                    );
+    uut: ULA port map(  
+        input1 => input1,
+        input2 => input2,
+        opselect => opselect,
+        output1 => output1,
+        output_greater_equal_or_signal => output_greater_equal_or_signal,
+        output_zero => output_zero
+    );
     process
     begin
     -------------------------------------------------
     ------------------ Sum tests --------------------
+        -- Sum with 0 with 0
+        input1 <= "0000000000000000";
+        input2 <= "0000000000000000";
+        opselect <= "00";
+        wait for 50 ns;
+        -- Sum with 0
+        input1 <= "0000000000000000";
+        input2 <= "0000000001110001";
+        opselect <= "00";
+        wait for 50 ns;
+        -- Sum with 1
+        input1 <= "0000000000000001";
+        input2 <= "0000000001110001";
+        opselect <= "00";
+        wait for 50 ns;
+        -- Sum 1 with 1
+        input1 <= "0000000000000001";
+        input2 <= "0000000000000001";
+        opselect <= "00";
+        wait for 50 ns;
+        -- Sum with 0
+        input1 <= "0001110001110001";
+        input2 <= "0000000000000000";
+        opselect <= "00";
+        wait for 50 ns;
+        -- Sum with 1
+        input1 <= "0001110001110010";
+        input2 <= "0000000000000001";
+        opselect <= "00";
+        wait for 50 ns;
+        -- Sum -1 with -1
+        input1 <= "1111111111111111";
+        input2 <= "1111111111111111";
+        opselect <= "00";
+        wait for 50 ns;
+        -- Sum with -1
+        input1 <= "0001111101110010";
+        input2 <= "1111111111111111";
+        opselect <= "00";
+        wait for 50 ns;
+        -- Sum with -1
+        input1 <= "1110010011001001";
+        input2 <= "1111111111111111";
+        opselect <= "00";
+        wait for 50 ns;
         -- Sum with both positives
-        input1 <= "0000100000001000";
+        input1 <= "0000000000000100";
         input2 <= "0000000101110011";
+        opselect <= "00";
+        wait for 50 ns;
+        -- Sum with both positives
+        input1 <= "0100000000000000";
+        input2 <= "0000011101011101";
+        opselect <= "00";
+        wait for 50 ns;
+        -- Sum with both positives
+        input1 <= "0000100010001000";
+        input2 <= "0000000101110011";
+        opselect <= "00";
+        wait for 50 ns;
+        -- Sum with positive and negative
+        input1 <= "1111111111111010";
+        input2 <= "0011011010010000";
+        opselect <= "00";
+        wait for 50 ns;
+        -- Sum with positive and negative
+        input1 <= "0011011010010100";
+        input2 <= "1111000000000000";
         opselect <= "00";
         wait for 50 ns;
         -- Sum with positive and negative
@@ -48,12 +114,52 @@ begin
         opselect <= "00";
         wait for 50 ns;
         -- Sum with both negatives
+        input1 <= "1111111111111111";
+        input2 <= "1111111111111111";
+        opselect <= "00";
+        wait for 50 ns;
+        -- Sum with both negatives
+        input1 <= "1111111111111011";
+        input2 <= "1111111111111111";
+        opselect <= "00";
+        wait for 50 ns;
+        -- Sum with both negatives
         input1 <= "1110111011000011";
-        input2 <= "1111111111111011";
+        input2 <= "1111111111111101";
+        opselect <= "00";
+        wait for 50 ns;
+        -- Sum with both negatives
+        input1 <= "1111111110101010";
+        input2 <= "1100111111111101";
         opselect <= "00";
         wait for 50 ns;
     -------------------------------------------------
     --------------- Subtraction tests ---------------
+        -- Subtraction with 0
+        input1 <= "0001111111111111";
+        input2 <= "0000000000000000";
+        opselect <= "01";
+        wait for 50 ns;
+        -- Subtraction with 1
+        input1 <= "0001111111111111";
+        input2 <= "0000000000000001";
+        opselect <= "01";
+        wait for 50 ns;
+        -- Subtraction with 10
+        input1 <= "0001111111111111";
+        input2 <= "0000000000001010";
+        opselect <= "01";
+        wait for 50 ns;
+        -- Subtraction with -1
+        input1 <= "0001111111111111";
+        input2 <= "1111111111111111";
+        opselect <= "01";
+        wait for 50 ns;
+        -- Subtraction with -5
+        input1 <= "0001111111111111";
+        input2 <= "1111111111111011";
+        opselect <= "01";
+        wait for 50 ns;
         -- Both positive, input1 > input2
         input1 <= "0001111111111111";
         input2 <= "0000000000011110";
